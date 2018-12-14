e1eb500d9530792312b0389b39151ae8b2b8e219
Thomas Mårtensson <thomas.martensson@redbridge.se>
2018-12-14 10:40:06 +0100

    Do not manage firewall, that is ansbiles job

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,6 +6,7 @@ node "lkl5502.lkl.ltkalmar.se" {
   # Configure puppetdb and its underlying database
   class { 'puppetdb':
       listen_address => '0.0.0.0'
+      manage_firewall => False
   }
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
