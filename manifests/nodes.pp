d743e8c7788e92f78e63501b10d6b70def7828c5
Thomas Mårtensson <thomas.martensson@redbridge.se>
2018-12-14 14:42:44 +0100

    Remove puppetdb

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,13 +2,3 @@ node default {
   include 'motd'
 }
 
-node "lkl5502.lkl.ltkalmar.se" {
-  # Configure puppetdb and its underlying database
-  class { 'puppetdb':
-      listen_address => '0.0.0.0',
-      manage_firewall => 'false'
-  }
-  # Configure the Puppet master to use puppetdb
-  class { 'puppetdb::master::config': }
-}
-
