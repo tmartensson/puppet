372fd82295b3c598a4259a8823f9a097d329324d
Thomas Mårtensson <thomas.martensson@redbridge.se>
2018-12-14 10:46:29 +0100

    ADd that comma

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -5,7 +5,7 @@ node default {
 node "lkl5502.lkl.ltkalmar.se" {
   # Configure puppetdb and its underlying database
   class { 'puppetdb':
-      listen_address => '0.0.0.0'
+      listen_address => '0.0.0.0',
       manage_firewall => False
   }
   # Configure the Puppet master to use puppetdb
