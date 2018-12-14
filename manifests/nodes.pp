499bbf4f99c16618cb9b61c52b1c8f616b89f263
Thomas Mårtensson <thomas.martensson@redbridge.se>
2018-12-14 10:47:41 +0100

    Fnuttologi

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,7 +6,7 @@ node "lkl5502.lkl.ltkalmar.se" {
   # Configure puppetdb and its underlying database
   class { 'puppetdb':
       listen_address => '0.0.0.0',
-      manage_firewall => False
+      manage_firewall => 'false'
   }
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
